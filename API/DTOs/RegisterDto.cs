2b67c6d5ce3c399f8caf7e4e6c15c5fe0c488f8f
Manuchehr <manoujon@gmail.com>
2026-07-22 12:40:13 -0500

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -6,9 +6,11 @@ public class RegisterDto
 {
     [Required]
     public string DisplayName { get; set; } = "";
+
     [Required]
     [EmailAddress]
     public string Email { get; set; } = "";
+
     [Required]
     [MinLength(4)]
     public string Password { get; set; } = "";
